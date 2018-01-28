ee65f0fed88ffbcc05c7b4ae0b97fb1f6a33a22c
Mohammad Farooqi <mohammad.farooqi@gmail.com>
2018-01-28 11:46:08 -0500

    route creeation complete - prescriptions and transfer

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -4,4 +4,70 @@ module.exports = function (router) {
   router.get('/', (req, res) => {
     return res.json({ message: 'Test Hello World Route!' });   
   });
+
+  router.get('/transferRequests', (req, res) => {
+    // scenario 1
+    const requests = [
+      {
+        status: 'new',
+        patientName: 'Mohammad',
+        drugName: 'Drug Mohammad'
+      },
+      {
+        status: 'inProgress',
+        patientName: 'Farooqi',
+        drugName: 'Drug Farooqi'
+      },
+      {
+        status: 'archived',
+        patientName: 'Mohammad Farooqi',
+        drugName: 'Drug Mohammad Farooqi'
+      },
+    ];
+
+    // scenario 2
+    // const requests = [
+    //   {
+    //     status: 'new',
+    //     patientName: 'Mohammad',
+    //     drugName: 'Drug Mohammad'
+    //   }
+    // ];
+    // const requests = [];
+
+    return res.json(requests);
+  });
+
+  router.get('/prescriptionOrders', (req, res) => {
+    // scenario 1
+    const requests = [
+      {
+        status: 'new',
+        patientName: 'Ayesha',
+        drugName: 'Drug Ayesha'
+      },
+      {
+        status: 'inProgress',
+        patientName: 'Izhar',
+        drugName: 'Drug Izhar'
+      },
+      {
+        status: 'archived',
+        patientName: 'Ayesha Izar',
+        drugName: 'Drug Ayesha Izhar'
+      },
+    ];
+
+    // scenario 2
+    // const requests = [];
+    // const requests = [
+    //   {
+    //     status: 'new',
+    //     patientName: 'Mohammad',
+    //     drugName: 'Drug Mohammad'
+    //   }
+    // ];
+
+    return res.json(requests);
+  });
 };
